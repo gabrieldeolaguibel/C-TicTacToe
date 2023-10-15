34c573437c963bbe6ff4e2901f42504e1263ab84
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2023-10-15 16:33:24 +0200

    Added Winning check test

--- tests.c
+++ tests.c
@@ -22,6 +22,41 @@ void test_isValidMove(){
     CU_ASSERT_FALSE(isValidMove(4, 4)); 
 }
 
+// tes for checkWin function from tic_tac_toe.c
+void test_checkWin(){
+    // test win condition for rows
+    initializeBoard();
+    board[0][0] = 'X';
+    board[0][1] = 'X';
+    board[0][2] = 'X';
+    CU_ASSERT_TRUE(checkWin('X'));
+    CU_ASSERT_FALSE(checkWin('O'))
+
+    // test win condition for columns
+    initializeBoard();
+    board[0][0] = 'O';
+    board[1][0] = 'O';
+    board[2][0] = 'O';
+    CU_ASSERT_TRUE(checkWin('O'));
+    CU_ASSERT_FALSE(checkWin('X'));
+
+    // Test win condition for diagonals
+    initializeBoard();
+    board[0][0] = 'X';
+    board[1][1] = 'X';
+    board[2][2] = 'X';
+    CU_ASSERT_TRUE(checkWin('X'));
+    CU_ASSERT_FALSE(checkWin('O'));
+
+    initializeBoard();
+    board[0][2] = 'O';
+    board[1][1] = 'O';
+    board[2][0] = 'O';
+    CU_ASSERT_TRUE(checkWin('O'));
+    CU_ASSERT_FALSE(checkWin('X'));
+
+}
+
 int main() {
     // Initialize CUnit test registry
     if (CUE_SUCCESS != CU_initialize_registry()) {
@@ -34,7 +69,8 @@ int main() {
        return CU_get_error();
     }
     // add the test funtin to the suite
-    if (NULL == CU_add_test(suite, "Test isValidMove function", test_isValidMove)) {
+    if (NULL == CU_add_test(suite, "Test isValidMove function", test_isValidMove) ||
+        NULL == CU_add_test(suite, "Test Check win function", test_checkWin)) {
         CU_cleanup_registry(); 
         return CU_get_error();
     }
